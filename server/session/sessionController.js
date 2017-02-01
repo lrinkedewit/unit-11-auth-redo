a6653e0658b3f3a15091cf4c392f683f15985d25
Brandon Mizuno <ranmizu@bair.local>
2017-02-01 10:35:35 -0800

    (feat) ask student to define method signatures

--- server/session/sessionController.js
+++ server/session/sessionController.js
@@ -6,24 +6,20 @@ const sessionController = {};
 * isLoggedIn - find the appropriate session for this request in the database, then
 * verify whether or not the session is still valid.
 *
-* @param req - http.IncomingRequest
-* @param res - http.ServerResponse
-* @param next - Callback with signature ([err])
+*
 */
-sessionController.isLoggedIn = (req, res, next) => {
+sessionController.isLoggedIn = (/* TODO: add parameters here */) => {
   // write code here
 
-  next();
 };
 
 /**
 * startSession - create a new Session model and then save the new session to the
 * database.
 *
-* @param cookieId - id to use as the id of the new session
-* @param callback - Callback with signature (Session)
+*
 */
-sessionController.startSession = (cookieId, callback) => {
+sessionController.startSession = (/* TODO: add parameters here */) => {
   //write code here
 
 };
