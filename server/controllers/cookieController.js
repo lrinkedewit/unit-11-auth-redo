1a72ad968123386e716c1f26e0da2f441da3d5d8
Augustine Kim <ajk830@gmail.com>
2019-09-23 15:56:49 -0700

    refactor folder structure and boilerplate to better reflect express middleware flow

---
+++ server/controllers/cookieController.js
@@ -0,0 +1,19 @@
+const cookieController = {};
+
+/**
+* setCookie - set a cookie with a random number
+*/
+cookieController.setCookie = (req, res, next) => {
+  // write code here
+
+}
+
+/**
+* setSSIDCookie - store the user id in a cookie
+*/
+cookieController.setSSIDCookie = (req, res, next) => {
+  // write code here
+
+}
+
+module.exports = cookieController;
